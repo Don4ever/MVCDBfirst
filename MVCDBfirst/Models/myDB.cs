b7031f87359545c9f6a203cf4c4b42f59cd7acb2
Don4ever <qassem.albahdour@gmail.com>
2017-10-30 17:23:53 +0400

    after addin myDB

---
+++ MVCDBfirst/Models/myDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCDBfirst.Models
+{
+    public class myDB : DbContext
+    {
+
+        public myDB() :base("DBConnection")
+        {
+            
+        }
+        public DbSet<user> users { get; set; }
+        public DbSet<post> posts { get; set; }
+    }
+}
