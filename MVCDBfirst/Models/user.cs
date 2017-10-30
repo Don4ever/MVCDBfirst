7808a7246d3410a2392276cf0a7db55bfe76751a
Don4ever <qassem.albahdour@gmail.com>
2017-10-30 17:14:50 +0400

    Adding user class

---
+++ MVCDBfirst/Models/user.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCDBfirst.Models
+{
+    public class user
+    {
+        [Key]
+        public int user_id { get; set; }
+        public string user_name { get; set; }
+        public string password { get; set; }
+    }
+}
