6680f0e6e10d91ed0b77b69f9d0e7470834737c3
Don4ever <qassem.albahdour@gmail.com>
2017-10-30 17:19:07 +0400

    after adding post class

---
+++ MVCDBfirst/Models/post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCDBfirst.Models
+{
+    public class post
+    {
+        [Key]
+        public int post_id { get; set; }
+        public string post_title { get; set; }
+        public string author { get; set; }
+    }
+}
