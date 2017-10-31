f8d37c14c6c888f13ce2fb5b6f7d89dbcc9bca6f
Don4ever <qassem.albahdour@gmail.com>
2017-10-31 17:55:08 +0400

    After adding viewModel "test Controller"

---
+++ MVCDBfirst/Controllers/testController.cs
@@ -0,0 +1,25 @@
+﻿using MVCDBfirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDBfirst.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: test
+        public ActionResult Index()
+        {
+
+            user emp = new user();
+            emp.user_id = 20000;
+            emp.password = "Marla";
+            emp.user_name = "Sukesh";
+
+            ViewData["Employee"] = emp;
+            return View("test");
+        }
+    }
+}
