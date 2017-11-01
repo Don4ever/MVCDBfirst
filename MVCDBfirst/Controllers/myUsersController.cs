9b8f86c52aaf5943447bc44838c1a060269a1706
Don4ever <qassem.albahdour@gmail.com>
2017-11-01 19:35:31 +0400

    After Completing view model example

--- MVCDBfirst/Controllers/myUsersController.cs
+++ MVCDBfirst/Controllers/myUsersController.cs
@@ -1,4 +1,5 @@
 ﻿using MVCDBfirst.Models;
+using MVCDBfirst.Models.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,7 @@ namespace MVCDBfirst.Controllers
         // GET: myUsers
         public ActionResult Index()
         {
-            int t = 1;
+            
             List<user> u = ndb.users.Where(x => x.user_id == 1).ToList();
 
             // var result = bd.users.Where(x => x.user_id > 1).Distinct();
@@ -37,5 +38,32 @@ namespace MVCDBfirst.Controllers
 
             return View();
         }
+        public ActionResult GetEmpData()
+        {
+
+            List<user> u = ndb.users.Where(x => x.user_id == 1).ToList();
+
+            // var result = bd.users.Where(x => x.user_id > 1).Distinct();
+
+            var id = 0;
+            foreach (var i in u)
+            {
+                id = i.user_id;
+
+            }
+
+
+            emp e= new emp();
+            e.emp_id = u[0].user_id;
+            e.pass = u[0].password;
+            e.us_name = u[0].user_name;
+
+            
+
+
+
+            return View(e);
+        }
     }
+    
 }
---
+++ MVCDBfirst/Models/ViewModel/emp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDBfirst.Models.ViewModel
+{
+    public class emp
+    {
+        public int emp_id { get; set; }
+        public string us_name { get; set; }
+        public string pass { get; set; }
+    }
+}
