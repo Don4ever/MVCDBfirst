9be08a5db3b00c0b81875d2890431cb7870a06c9
Don4ever <qassem.albahdour@gmail.com>
2017-11-01 17:59:45 +0400

    ViewData ""myUsers"

--- MVCDBfirst/Controllers/myUsersController.cs
+++ MVCDBfirst/Controllers/myUsersController.cs
@@ -13,7 +13,7 @@ namespace MVCDBfirst.Controllers
         // GET: myUsers
         public ActionResult Index()
         {
-
+            int t = 1;
             List<user> u = ndb.users.Where(x => x.user_id == 1).ToList();
 
             // var result = bd.users.Where(x => x.user_id > 1).Distinct();
